b877a643dddaaf6ea449cc95f108185813978cfb
Json <280493597@qq.com>
2019-09-16 11:51:49 +0800

    加入创建数据库和表的配置

---
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -0,0 +1,42 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;
+    private int cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(int cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
